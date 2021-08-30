925741996572ea03be958f0e1cd5b33835b5e197
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-30 12:21:36 -0700

    working on html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/exercise", (req, res) => {
-  res.SendFile(path.join(___dirname, "../public/exercise.html"));
+  res.SendFile(path.join(___dirname, "/public/exercise.html"));
 });
 router.get("/stats", (req, res) => {
-  res.SendFile(path.join(___dirname, "../public/stats.html"));
+  res.SendFile(path.join(___dirname, "/public/stats.html"));
 });
 
 module.exports = router;
