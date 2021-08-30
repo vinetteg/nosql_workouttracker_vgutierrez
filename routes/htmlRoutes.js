36dae50ba1edcff89395f9e8fc74dafe7843199a
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-30 12:46:19 -0700

    correcting the smallest capitilization error that was stopping the app from running

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/exercise", (req, res) => {
-  res.SendFile(path.join(___dirname, "/public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 router.get("/stats", (req, res) => {
-  res.SendFile(path.join(___dirname, "/public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
